bf38b8de203fe65d415aa4065a3045b8c58862cb
Gagas Pradipta <118372630+N3wtz@users.noreply.github.com>
2024-04-08 11:17:38 +0800

    Add files via upload

---
+++ src/com/kruwell/program/Konfig.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kruwell.program;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author gagas
+ */
+public class Konfig {
+    
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB()throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/tugaspbo";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+            
+        }catch(SQLException e){
+            System.out.println("Koneksi ke Database Gagal " + e.getMessage());
+        }
+        return MySQLConfig;
+    }
+    
+}
